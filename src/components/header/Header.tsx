f6de44e79621316208f7cefe2d45c52c805c5bdf
pedrobarrosvp <nl.vppedro3@gmail.com>
2025-12-19 01:04:14 -0300

    estilo do header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getCatImage } from "../../services/api";
+import "./Header.css";
 
 const Header: React.FC = () => {
     const [catImage, setCatImage] = useState<string>("");
